0b845f06ccff2f3c859c8b6229a119c8fcb0ab23
KennyVillanueva <kennyvillanueva15@gmail.com>
2018-06-25 12:03:06 -0500

    first blood

---
+++ app.js
@@ -0,0 +1,13 @@
+function generateID(){
+
+    //firstName
+    let userFirstName =
+    document.getElementById("firstName").value;
+    document.getElementById("idCard").innerHTML = userFirstName;
+
+    // Last firstName
+    let userlastName =
+    document.getElementById("lastName").value;
+    document.getElementById("idCard").innerHTML = userlastName;
+    //Age
+}
